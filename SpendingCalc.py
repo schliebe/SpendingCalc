c1be55593adf28b90df60b318e95c6c1c52f9dbc
schliebe <schli_be@hotmail.de>
2021-01-05 03:45:52 +0100

    Create SpendingCalc.py
    Main script for the bot

---
+++ SpendingCalc.py
@@ -0,0 +1,32 @@
+from telegram.ext import Updater
+
+
+def load_config():
+    """Load data out of the config file and return as a dict.
+
+    :return: dict with keys and values in the config file
+    """
+    config = {'Telegram_Bot_Token': None}
+    with open('config.txt') as file:
+        for line in file:
+            key, value = line.strip().split('=', 1)
+            config[key] = value
+    return config
+
+
+def main():
+    # Daten aus Config-Datei laden
+    config = load_config()
+
+    # Bot erstellen und Token festlegen
+    updater = Updater(token=config['Telegram_Bot_Token'])
+    dispatcher = updater.dispatcher
+
+    # Bot starten
+    updater.start_polling()
+    print('Bot started!')
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
